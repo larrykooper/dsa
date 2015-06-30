da88e69e540c1da815b1c9dadd7e95d24a5a2441
Larry Kooper <larry.kooper@gmail.com>
2015-06-30 16:32:58 -0400

    Used sinon to spy on console.log

--- test/graph.js
+++ test/graph.js
@@ -1,5 +1,11 @@
 var Graph = require('../src/graph');
-var assert = require("assert");
+
+var chai = require('chai');
+var expect = chai.expect;
+var sinonChai = require('sinon-chai');
+var sinon = require('sinon');
+
+chai.use(sinonChai);
 
 var graph = new Graph(6);
 
@@ -8,5 +14,23 @@ graph.addEdge(0,2);
 graph.addEdge(0,3);
 graph.addEdge(2,4);
 graph.addEdge(3,5);
-graph.showGraph();
+//graph.showGraph();
+//graph.dfs(0);
 
+describe('graph dfs', function () {
+
+    beforeEach(function() {
+        sinon.spy(console, 'log');
+    });
+
+    afterEach(function() {
+        console.log.restore();
+    });
+
+    describe('dfs', function() {
+        it('visits all vertices', function () {
+            graph.dfs(0);
+            expect(console.log).to.have.callCount(6);
+        });
+    });
+});
