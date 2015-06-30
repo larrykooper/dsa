60821e4c96f234c229a6490c36d2a435a412540f
Larry Kooper <larry.kooper@gmail.com>
2015-06-30 11:13:23 -0400

    Implemented get for hash table

--- src/hashtable.js
+++ src/hashtable.js
@@ -1,10 +1,10 @@
 var LinkedList = require('./linkedlist');
 
-var b = 137;
+var tableLength = 137;
 
 HashTable = function () {
     var i;
-    this.table = new Array(b);
+    this.table = new Array(tableLength);
     for (i = 0; i < this.table.length; i++) {
         this.table[i] = new LinkedList();
     }
@@ -21,25 +21,34 @@ HashTable = function () {
  * p. 101
  * the number 37 comes from Horner's method
  */
-betterHash = function(string, hashtable) {
+hashFunc = function(string) {
     var i;
     const H = 37;
     var total = 0;
     for (i = 0; i < string.length; i++) {
         total += H * total + string.charCodeAt(i);
     }
-    total = total % hashtable.table.length;
+    total = total % tableLength;
     return parseInt(total);
 }
 
 HashTable.prototype.insert = function(key, data) {
-    var pos = betterHash(key, this);
+    var pos = hashFunc(key);
     var bucket = this.table[pos];
     bucket.insertAtBeginning(key, data);
 }
-
+// get
+// returns data for this key, or null if not found
+//
 HashTable.prototype.get = function(key) {
-
+    var pos = hashFunc(key);
+    var bucket = this.table[pos];
+    var node = bucket.find(key);
+    if (node) {
+        return node.refToData;
+    } else {
+        return null;
+    }
 }
 
 module.exports = HashTable;
